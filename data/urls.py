60fd891ffd466c0f42823d1ae9fb8f401e5a4b2e
aswinm <aswinmurugesh94@gmail.com>
2015-02-17 14:31:25 +0530

    Formatting changed

--- data/urls.py
+++ data/urls.py
@@ -6,6 +6,10 @@ urlpatterns = patterns('',
         url(r'^products/(?P<cid>\w+)/?$',views.products),
         url(r'^product/(?P<pid>.*)/?$',views.product),
         url(r'^sendmail/?$',views.sendmail),
+        url(r'^accessories/?$',views.accessories),
         url(r'^gallery/?$',views.gallery),
+        url(r'^services/?$',views.services),
+        url(r'^about/?$',views.about),
+        url(r'^contact/?$',views.contact),
 
         )
--- data/views.py
+++ data/views.py
@@ -15,6 +15,7 @@ def sendmail(request):
         det = request.POST
         msg = "NAME: " + det["name"]+"\n"
         msg += "EMAIL: "+det["email"]+"\n"
+        msg += "Contact Number: "+det["contact"]+"\n"
         msg += "SUBJECT: "+det["subject"]+"\n"
         msg += "MESSAGE: "+det["message"]+"\n"
         s = send_mail(det["subject"],msg,settings.EMAIL_HOST_USER,['sonyrigsmail@gmail.com'])
@@ -26,20 +27,31 @@ def sendmail(request):
         return HttpResponseRedirect("/")
 
 def products(request,cid):
+
     c = Category.objects.filter(url=cid).first()
     if not c:
         return HttpResponseRedirect('/')
     
+
     p = Product.objects.filter(cat__url=cid)
+
+    x = Product.objects.all()
+    prod_list = {}
+    prod_list["waterwellrigs"] = x.filter(cat__url="waterwellrigs")
+    prod_list["miningexplorationrigs"] = x.filter(cat__url="miningexplorationrigs")
     if p.count()>4:
         p_li = [p[:4],p[4:]]
     else:
         p_li = [p]
-    return render(request,"products_list.html",{'page_title': c.name,'product_list':p,'p_li':p_li})
+    return render(request,"products_list.html",{'page_title': c.name,'product_list':p,'p_li':p_li,'prod_list':prod_list})
 
 def product(request,pid):
     
     p = Product.objects.filter(url=pid).first()
+    x = Product.objects.all()
+    prod_list = {}
+    prod_list["waterwellrigs"] = x.filter(cat__url="waterwellrigs")
+    prod_list["miningexplorationrigs"] = x.filter(cat__url="miningexplorationrigs")
     if not p:
         return HttpResponseRedirect("/")
     table_data = []
@@ -48,8 +60,18 @@ def product(request,pid):
         x["name"],x["data"] = i.split(":")
         table_data.append(x)
 
-    return render(request,"product.html",{'prod':p,"content":table_data,'page_title':p.name})
+    return render(request,"product.html",{'prod':p,"content":table_data,'page_title':p.name,'prod_list':prod_list})
 
 
+def accessories(request):
+    return render(request,"accessories.html",{'page_title':"Accessories"})
 
+def services(request):
+    return render(request,"services.html",{'page_title':"Services"})
+
+def about(request):
+    return render(request,"about.html",{'page_title':"About Us"})
+
+def contact(request):
+    return render(request,"contact.html",{'page_title':"Contact Us"})
 # Create your views here.
